6d4e6289ec78d29940b84a755c9ff83f55a06808
orlando <musheddev@users.noreply.github.com>
2019-08-18 20:52:49 -0500

    Update build.fsx

--- build.fsx
+++ build.fsx
@@ -46,23 +46,11 @@ module DemoSettings =
         }
 
 
-    let mdl = "test"
-    let app = "integration"
 
-    let service = "expecto"
-
-    let version = "1.0.0"
-    let serviceType = ""
-    let pkg = mdl+"."+app+"."+service+"pkg"
-
-    let proj = @"src\ExpectoService\ExpectoService.fsproj"
-    let projF = @"src\ExpectoService"
+    let pkg = "TestPkg"
 
     let root = @"C:\Users\Orlando\Desktop\Projects2019\Fake.TurboFabric"
-    let buildOutput = ""
     let packagePrep = root +/+ "prep"
-    let package = ""
-    let dll = ""
 
     let ExpectoService = {
         Dll = "ExpectoService.dll"
@@ -78,7 +66,7 @@ module DemoSettings =
         Version = "1.0.0"
         Type = "TestApplicationType"
     }
-
+open DemoSettings
 module TurboFabric =
     open Microsoft.ServiceFabric.Client
     open Microsoft.ServiceFabric.Common.Security
@@ -96,46 +84,46 @@ module TurboFabric =
     let xElement expandedName content = XElement(xName expandedName, content |> Seq.map (fun v -> v :> obj) |> Seq.toArray) :> obj
     let xAttribute expandedName value = XAttribute(xName expandedName, value) :> obj
 
-    let serviceMani = 
+    let serviceMani (service : ServiceFabricService) = 
         xElement "ServiceManifest" [
-            xAttribute "Name" DemoSettings.pkg
-            xAttribute "Version" DemoSettings.version
+            xAttribute "Name" service.Name
+            xAttribute "Version" service.Version
             //xAttribute "xmlns" "http://schemas.microsoft.com/2011/01/fabric"
             //xAttribute "xmlns:xsd" "http://www.w3.org/2001/XMLSchema"
             //xAttribute "xmlns:xsi" "http://www.w3.org/2001/XMLSchema-instance"
             xElement "ServiceTypes" [
                 xElement "StatelessServiceType" [
-                    xAttribute "ServiceTypeName" DemoSettings.serviceType
+                    xAttribute "ServiceTypeName" service.Type
                 ]
             ]
             xElement "CodePackage" [
                 xAttribute "Name" "Code"
-                xAttribute "Version" DemoSettings.version
+                xAttribute "Version" service.Version
                 xElement "EntryPoint" [
                     xElement "ExeHost" [
                         xAttribute "IsExternalExecutable" true
                         xElement "Program" ["dotnet"]
-                        xElement "Arguments" [DemoSettings.dll]
+                        xElement "Arguments" [service.Dll]
                     ]
                 ]               
             ]
             xElement "ConfigPackage" [
                 xAttribute "Name" "Config"
-                xAttribute "Version" DemoSettings.version                
+                xAttribute "Version" service.Version                
             ]
         ]
 
-    let appMani = 
+    let appMani (application : ServiceFabricApplication) = 
         xElement "ApplicationManifest" [
-            xAttribute "ApplicationManifestName" DemoSettings.pkg
-            xAttribute "ApplicationManifestVersion" DemoSettings.version
+            xAttribute "ApplicationManifestName" application.Name
+            xAttribute "ApplicationManifestVersion" application.Version
             //xAttribute "xmlns" "http://schemas.microsoft.com/2011/01/fabric"
             //xAttribute "xmlns:xsd" "http://www.w3.org/2001/XMLSchema"
             //xAttribute "xmlns:xsi" "http://www.w3.org/2001/XMLSchema-instance"
             xElement "ServiceManifestImport" [
                 xElement "ServiceManifestRef" [
-                    xAttribute "ServiceManifestName" DemoSettings.pkg
-                    xAttribute "ServiceManifestVersion" DemoSettings.version
+                    xAttribute "ServiceManifestName" application.Services.[0].Name
+                    xAttribute "ServiceManifestVersion" application.Services.[0].Version
                 ]
             ]
         ]
@@ -147,33 +135,32 @@ module TurboFabric =
             //xAttribute "xmlns:xsi" "http://www.w3.org/2001/XMLSchema-instance"            
         ]
 
-    let serviceManifest = serviceMani :?> XElement |> ServiceManifest
-    let applicationManifest = appMani :?> XElement |> AppManifest
+    let serviceManifest = serviceMani ExpectoService :?> XElement |> ServiceManifest
+    let applicationManifest = appMani Application :?> XElement |> AppManifest
     let settings = configMani :?> XElement |> Settings
 
 
 
-    let makeFolders () =
-        Directory.ensure DemoSettings.packagePrep
-        Directory.ensure (DemoSettings.packagePrep +/+ DemoSettings.pkg)
-        Directory.ensure (DemoSettings.packagePrep +/+ DemoSettings.pkg +/+ "Config")
+    let makeFolders (application : ServiceFabricApplication) =
+        Directory.ensure packagePrep
+        Directory.ensure (packagePrep +/+ pkg)
+        Directory.ensure (packagePrep +/+ pkg +/+ "Config")
         
-        settings.XElement.Save(DemoSettings.packagePrep +/+ DemoSettings.pkg +/+ "Config" +/+ "Settings.xml")
-        serviceManifest.XElement.Save(DemoSettings.packagePrep  +/+ DemoSettings.pkg +/+ "ServiceManifest.xml")
-        applicationManifest.XElement.Save(DemoSettings.packagePrep +/+ "ApplicationManifest.xml")
+        settings.XElement.Save(packagePrep +/+ pkg +/+ "Config" +/+ "Settings.xml")
+        serviceManifest.XElement.Save(packagePrep  +/+ pkg +/+ "ServiceManifest.xml")
+        applicationManifest.XElement.Save(packagePrep +/+ "ApplicationManifest.xml")
 
-        Directory.ensure (DemoSettings.packagePrep +/+ DemoSettings.pkg +/+ "Code")
+        Directory.ensure (packagePrep +/+ pkg +/+ "Code")
         //copy build
 
-    let build () =
-        let workingDir = DemoSettings.root +/+ DemoSettings.projF
+    let build (application : ServiceFabricApplication) =
         let result =
-            DotNet.exec (DotNet.Options.withWorkingDirectory workingDir) "build" ""
-        if result.ExitCode <> 0 then failwithf "'dotnet %s' failed in %s" "build" workingDir
+            DotNet.exec (DotNet.Options.withWorkingDirectory root) "build" application.Services.[0].Proj
+        if result.ExitCode <> 0 then failwithf "'dotnet %s' failed in %s" "build" root
 
         let result =
-            DotNet.exec (DotNet.Options.withWorkingDirectory workingDir) "publish" (" -o " + DemoSettings.packagePrep +/+ DemoSettings.pkg +/+ "Code")
-        if result.ExitCode <> 0 then failwithf "'dotnet %s' failed in %s" "publish" workingDir
+            DotNet.exec (DotNet.Options.withWorkingDirectory root) "publish" (application.Services.[0].Proj + " -o " + packagePrep +/+ pkg +/+ "Code")
+        if result.ExitCode <> 0 then failwithf "'dotnet %s' failed in %s" "publish" root
     // let zip () =
     //     [   !! "ci/build/project1/**/*"
     //         |> Zip.filesAsSpecs "ci/build/project1"
@@ -242,8 +229,9 @@ Target.create "Clean" (fun _ ->
 Target.create "Build" (fun _ ->
     // !! "src/**/*.*proj"
     // |> Seq.iter (DotNet.build id)
-    TurboFabric.makeFolders()
-    TurboFabric.build()
+    let app = Application
+    TurboFabric.makeFolders app
+    TurboFabric.build app
 )
 
 Target.create "All" ignore
