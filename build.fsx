346493fe9f22e5c046671faa0781f150f6adfade
orlando <musheddev@users.noreply.github.com>
2019-08-19 20:20:38 -0500

    update

--- build.fsx
+++ build.fsx
@@ -19,6 +19,7 @@ open Fake.Core.Xml
 open Fake.Core.TargetOperators
 open Fake.IO.Globbing.Operators
 
+
 open System.Threading.Tasks
 open System.Collections.Generic
 open System
@@ -26,7 +27,21 @@ open System.Threading
 
 let (+/+) = Path.combine
 
-module DemoSettings =
+let root = @"C:\Users\Orlando\Desktop\Projects2019\Fake.TurboFabric"
+
+
+module TurboFabric =
+    open Microsoft.ServiceFabric.Client
+    open Microsoft.ServiceFabric.Common.Security
+    open Microsoft.ServiceFabric.Client.Http
+    open FSharp.Data
+    open System.Xml.Linq
+
+    type Schema = XmlProvider<Schema = "C:\\Users\\Orlando\\Desktop\\Projects2019\\Fake.TurboFabric\\ServiceFabricServiceModel.xsd">
+
+    type AppManifest = Schema.ApplicationManifest
+    type ServiceManifest = Schema.ServiceManifest
+    type Settings = Schema.Settings
 
     type ServiceFabricService = 
         {
@@ -45,40 +60,15 @@ module DemoSettings =
             Services : ServiceFabricService []
         }
 
+    type Creds =
+        {
+            Endpoint : string
+            PfxPath : string
+            Pass : string
+            Name : string
+            Thumbprint : string
+        }    
 
-
-    let pkg = "TestPkg"
-
-    let root = @"C:\Users\Orlando\Desktop\Projects2019\Fake.TurboFabric"
-    let packagePrep = root +/+ "prep"
-
-    let ExpectoService = {
-        Dll = "ExpectoService.dll"
-        Name = "Expecto"
-        Proj = @"src\ExpectoService\ExpectoService.fsproj"
-        Version = "1.0.0"
-        Type = "ExpectoServiceType"
-    }
-
-    let Application = {
-        Services = [|ExpectoService|]
-        Name = "Test"
-        Version = "1.0.0"
-        Type = "TestApplicationType"
-    }
-open DemoSettings
-module TurboFabric =
-    open Microsoft.ServiceFabric.Client
-    open Microsoft.ServiceFabric.Common.Security
-    open Microsoft.ServiceFabric.Client.Http
-    open FSharp.Data
-    open System.Xml.Linq
-
-    type Schema = XmlProvider<Schema = "C:\\Users\\Orlando\\Desktop\\Projects2019\\Fake.TurboFabric\\ServiceFabricServiceModel.xsd">
-
-    type AppManifest = Schema.ApplicationManifest
-    type ServiceManifest = Schema.ServiceManifest
-    type Settings = Schema.Settings
 
     let xName expandedName = XName.Get(expandedName)
     let xElement expandedName content = XElement(xName expandedName, content |> Seq.map (fun v -> v :> obj) |> Seq.toArray) :> obj
@@ -135,25 +125,25 @@ module TurboFabric =
             //xAttribute "xmlns:xsi" "http://www.w3.org/2001/XMLSchema-instance"            
         ]
 
-    let serviceManifest = serviceMani ExpectoService :?> XElement |> ServiceManifest
-    let applicationManifest = appMani Application :?> XElement |> AppManifest
+    let serviceManifest expectoService = serviceMani expectoService :?> XElement |> ServiceManifest
+    let applicationManifest application = appMani application :?> XElement |> AppManifest
     let settings = configMani :?> XElement |> Settings
 
 
 
-    let makeFolders (application : ServiceFabricApplication) =
+    let makeFolders (application : ServiceFabricApplication) packagePrep pkg=
         Directory.ensure packagePrep
         Directory.ensure (packagePrep +/+ pkg)
         Directory.ensure (packagePrep +/+ pkg +/+ "Config")
         
         settings.XElement.Save(packagePrep +/+ pkg +/+ "Config" +/+ "Settings.xml")
-        serviceManifest.XElement.Save(packagePrep  +/+ pkg +/+ "ServiceManifest.xml")
-        applicationManifest.XElement.Save(packagePrep +/+ "ApplicationManifest.xml")
+        (serviceManifest application.Services.[0]).XElement.Save(packagePrep  +/+ pkg +/+ "ServiceManifest.xml")
+        (applicationManifest application).XElement.Save(packagePrep +/+ "ApplicationManifest.xml")
 
         Directory.ensure (packagePrep +/+ pkg +/+ "Code")
-        //copy build
 
-    let build (application : ServiceFabricApplication) =
+
+    let build (application : ServiceFabricApplication) packagePrep pkg =
         let result =
             DotNet.exec (DotNet.Options.withWorkingDirectory root) "build" application.Services.[0].Proj
         if result.ExitCode <> 0 then failwithf "'dotnet %s' failed in %s" "build" root
@@ -161,31 +151,29 @@ module TurboFabric =
         let result =
             DotNet.exec (DotNet.Options.withWorkingDirectory root) "publish" (application.Services.[0].Proj + " -o " + packagePrep +/+ pkg +/+ "Code")
         if result.ExitCode <> 0 then failwithf "'dotnet %s' failed in %s" "publish" root
-    // let zip () =
-    //     [   !! "ci/build/project1/**/*"
-    //         |> Zip.filesAsSpecs "ci/build/project1"
-    //         |> Zip.moveToFolder "project1" ]
-    //     |> Seq.concat
-    //     |> Zip.zipSpec (sprintf @"ci/deploy/project.%s.zip" buildVersion)    
+    
+    let zip packagePrep =
+        [   "", !! (packagePrep + "/**/*") ]
+        |> Zip.zipOfIncludes "deploy.sfpkg"   
 
-    let getCreds (pfxPath : string) name thumbprint (pass : string) (ct : CancellationToken) =
-            let clientCert = new System.Security.Cryptography.X509Certificates.X509Certificate2(pfxPath, pass)
-            let name = X509Name(name,thumbprint)
+    let getCreds (creds : Creds) ct =
+            let clientCert = new System.Security.Cryptography.X509Certificates.X509Certificate2(creds.PfxPath, creds.Pass)
+            let name = X509Name(creds.Name,creds.Thumbprint)
             let remoteSecuritySettings = RemoteX509SecuritySettings(List<X509Name>([name]))
             Task.FromResult<SecuritySettings>(X509SecuritySettings(clientCert, remoteSecuritySettings))
 
-    let connect cluster getcreds =
+    let connect creds =
         let sfClient = ServiceFabricClientBuilder()
-                        .UseEndpoints(Uri(@"https://"+cluster+":19080"))
-                        .UseX509Security(Func<CancellationToken,Task<SecuritySettings>>(getcreds))
+                        .UseEndpoints(Uri(creds.Endpoint))
+                        .UseX509Security(Func<CancellationToken,Task<SecuritySettings>>(getCreds creds))
                         .BuildAsync().GetAwaiter().GetResult();
         sfClient
 
     let push (client : IServiceFabricClient) path storePath =
         let ct = CancellationToken()
-        client.ImageStore.UploadApplicationPackageAsync(path,true,storePath,ct)
+        client.ImageStore.UploadApplicationPackageAsync(path,true,storePath,ct).GetAwaiter().GetResult()
 
-    let start (client : IServiceFabricClient) appType =
+    let start (client : IServiceFabricClient) appType appName =
         let ct = CancellationToken()
         let appParams = Dictionary<string, string>()
         let appDesc = Microsoft.ServiceFabric.Common.ApplicationDescription(Microsoft.ServiceFabric.Common.ApplicationName("fabric:/" + ""), appType, "1.0.0", appParams);
@@ -194,6 +182,37 @@ module TurboFabric =
     let provision (client : IServiceFabricClient) appType =
         client.ApplicationTypes.ProvisionApplicationTypeAsync(Microsoft.ServiceFabric.Common.ProvisionApplicationTypeDescription(appType))
 
+
+module DemoSettings =
+    open TurboFabric
+    let pkg = "TestPkg"
+
+    let root = @"C:\Users\Orlando\Desktop\Projects2019\Fake.TurboFabric"
+    let packagePrep = root +/+ "prep"
+
+    let ExpectoService = {
+        Dll = "ExpectoService.dll"
+        Name = "Expecto"
+        Proj = @"src\ExpectoService\ExpectoService.fsproj"
+        Version = "1.0.0"
+        Type = "ExpectoServiceType"
+    }
+
+    let Application = {
+        Services = [|ExpectoService|]
+        Name = "Test"
+        Version = "1.0.0"
+        Type = "TestApplicationType"
+    }
+
+    let Creds = {
+        Endpoint = @"https://mushed.eastus2.cloudapp.azure.com:19000"
+        PfxPath = @"C:\Users\Orlando\Downloads\mushed-mushed-client-20190820.pfx"
+        Pass = ""
+        Name = "mushedclient"
+        Thumbprint = "BA256D31BC18BA647CEFF1498CE7E6FE63400021"
+        //cluster id a6611203-9586-4e9a-996a-447f5dea257c
+    }
 
 let runDotNet cmd workingDir =
     let result =
@@ -229,9 +248,15 @@ Target.create "Clean" (fun _ ->
 Target.create "Build" (fun _ ->
     // !! "src/**/*.*proj"
     // |> Seq.iter (DotNet.build id)
-    let app = Application
-    TurboFabric.makeFolders app
-    TurboFabric.build app
+    let app = DemoSettings.Application
+    TurboFabric.makeFolders app DemoSettings.packagePrep DemoSettings.pkg
+    TurboFabric.build app DemoSettings.packagePrep DemoSettings.pkg
+    TurboFabric.zip DemoSettings.packagePrep
+
+    //let client = TurboFabric.connect DemoSettings.Creds
+    //TurboFabric.push client DemoSettings.packagePrep  DemoSettings.pkg
+
+    ()
 )
 
 Target.create "All" ignore
