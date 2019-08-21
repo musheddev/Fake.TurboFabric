d91c500e6e08879fad7f7042bacc6da78172b036
dandereggK <42975112+dandereggK@users.noreply.github.com>
2019-08-21 09:52:48 -0500

    show error

--- build.fsx
+++ build.fsx
@@ -25,7 +25,7 @@ open System.Threading.Tasks
 open System.Collections.Generic
 open System
 open System.Threading
-open System.Management.Automation
+//open System.Management.Automation
 
 let (+/+) = Path.combine
 
@@ -214,10 +214,10 @@ module DemoSettings =
         //cluster id a6611203-9586-4e9a-996a-447f5dea257c
     }
 
-let runDotNet cmd workingDir =
-    let result =
-        DotNet.exec (DotNet.Options.withWorkingDirectory workingDir) cmd ""
-    if result.ExitCode <> 0 then failwithf "'dotnet %s' failed in %s" cmd workingDir
+// let runDotNet cmd workingDir =
+//     let result =
+//         DotNet.exec (DotNet.Options.withWorkingDirectory workingDir) cmd ""
+//     if result.ExitCode <> 0 then failwithf "'dotnet %s' failed in %s" cmd workingDir
 
 
 // Target.create "Run" (fun _ ->
@@ -236,24 +236,79 @@ let runDotNet cmd workingDir =
 //     |> Async.RunSynchronously
 //     |> ignore
 // )
+// type System.String with
+//     static member Delimit delimiter (items:string seq) =
+//         String.Join(delimiter,items |> Array.ofSeq)
 
-let retCode =
-    ExecProcess
-      (fun info ->
-        info.Name <- "powershell.exe"  // Don't know if you need full path here
-        info.WorkingDirectory <- getBuildParam "BuildRoot"
-        info.Arguments <-
-          [ "-File"; getBuildParam "BuildRoot" + "\DeployScripts\scripts\AdminScripts\VersionUpdateFile.ps1" |> inQuotes;
-            "-path"; getBuildParam "BuildSolutionVersioningConfig" |> inQuotes;
-            "-majorVersion"; getBuildParam "BuildNumberMajor" |> inQuotes;
-            "-minor"; getBuildParam "BuildNumberMinor" |> inQuotes;
-            "-build"; getBuildParam "BuildNumber" |> inQuotes;
-            "-revision"; getBuildParam "BuildNumberRevision" |> inQuotes
-          ] |> separated " "
-      )
-      (TimeSpan.FromMinutes 5.0)
-  if retCode <> 0 then
-    failwith (sprintf "PowerShell exited with non-zero exit code %d" retCode)
+// module Proc =
+//     // ExecProcess is a thin wrapper around Process.Start
+//     // docs at
+//     //   https://github.com/fsharp/FAKE/blob/master/src/app/FakeLib/ProcessHelper.fs#L184-L185
+//     //   https://github.com/fsharp/FAKE/blob/master/src/app/FakeLib/ProcessHelper.fs#L68-L107
+//     let ExecProcessElevated cmd args timeOut =
+//         ExecProcess (fun si ->
+//             si.Verb <- "runas"
+//             si.Arguments <- args
+//             si.FileName <- cmd
+//             si.UseShellExecute <- true) timeOut
+//   // wrapper for fake built-in in case we want the entire process results, not just the exitcode
+//   let runElevated cmd args timeOut =
+//       let tempFilePath = System.IO.Path.GetTempFileName()
+//       // hacky work around for not being able to capture output of an elevated process
+//       let resultCode = ExecProcessElevated "cmd" (sprintf "/c %s %s > %s" cmd args tempFilePath) timeOut
+//       trace "reading output results of runElevated"
+//       let outputResults = File.ReadAllLines tempFilePath
+//       File.Delete tempFilePath
+//       // idiomaticisizing the result, not needed, especially for uses outside of FAKE
+//       let processResult = ProcessResult.New resultCode (ResizeArray<_> outputResults) (ResizeArray<_>())
+//       (String.Delimit "\r\n" outputResults)
+//       |> trace
+//       processResult
+
+// module Sec =
+//         open System.Security.Principal
+//         let getIsAdmin() =
+//             WindowsIdentity.GetCurrent()
+//             |> WindowsPrincipal
+//             |> fun wp -> wp.IsInRole(WindowsBuiltInRole.Administrator)
+//         let requireAdmin () =
+//             let runningAsAdmin = getIsAdmin()
+//             if not runningAsAdmin then
+//                 failwithf "Can't alter hosts file without admin permissions"
+
+// Sec.requireAdmin()
+
+// let retCode =
+//     ExecProcess
+//       (fun info ->
+//         info.Name <- "powershell.exe"  // Don't know if you need full path here
+//         info.Verb <- "runas"
+//         info.WorkingDirectory <- getBuildParam "BuildRoot"
+//         info.UseShellExecute
+//         info.Arguments <-
+//           [ "-File"; getBuildParam "BuildRoot" + "\DeployScripts\scripts\AdminScripts\VersionUpdateFile.ps1" |> inQuotes;
+//             "-path"; getBuildParam "BuildSolutionVersioningConfig" |> inQuotes;
+//             "-majorVersion"; getBuildParam "BuildNumberMajor" |> inQuotes;
+//             "-minor"; getBuildParam "BuildNumberMinor" |> inQuotes;
+//             "-build"; getBuildParam "BuildNumber" |> inQuotes;
+//             "-revision"; getBuildParam "BuildNumberRevision" |> inQuotes
+//           ] |> separated " "
+//       )
+//       (TimeSpan.FromMinutes 5.0)
+//   if retCode <> 0 then
+//     failwith (sprintf "PowerShell exited with non-zero exit code %d" retCode)
+
+// let pshell =
+//         let pipe=PowerShell.Create()
+//             .AddCommand("invoke-command")
+//         pipe.AddParameter("ComputerName", server)
+//             .Invoke()
+//             |> Seq.map (sprintf  "%O")
+//             |> Seq.iter (fun line -> if line.Contains("not installed") then traceError line else trace line)
+//         pipe.Streams.Error |> Seq.iter (traceError << sprintf "%O" )
+
+
+//let connect creds =
 
 
 Target.create "Clean" (fun _ ->
@@ -271,7 +326,7 @@ Target.create "Build" (fun _ ->
     TurboFabric.build app DemoSettings.packagePrep DemoSettings.pkg
     TurboFabric.zip DemoSettings.packagePrep
 
-    //let client = TurboFabric.connect DemoSettings.Creds
+    let client = TurboFabric.connect DemoSettings.Creds
     //TurboFabric.push client DemoSettings.packagePrep  DemoSettings.pkg
 
     ()
--- src/Fake/Library.fs
+++ src/Fake/Library.fs
@@ -9,24 +9,144 @@ open System.Collections.Generic
 open Microsoft.ServiceFabric.Client
 open System
 open System.Threading
-
-
+open Microsoft.ServiceFabric.Client
+open Microsoft.ServiceFabric.Common.Security
+open Microsoft.ServiceFabric.Client.Http
+open FSharp.Data
+open System.Xml.Linq
 
 module Fake =
-    let getCreds (pfxPath : string) name thumbprint (pass : string) (ct : CancellationToken) =
-            let clientCert = new System.Security.Cryptography.X509Certificates.X509Certificate2(pfxPath, pass)
-            let name = X509Name(name,thumbprint)
+
+    type Schema = XmlProvider<Schema = @"C:\Users\danderegg\Desktop\Projects\Fake.TurboFabric\ServiceFabricServiceModel.xsd">
+
+    type AppManifest = Schema.ApplicationManifest
+    type ServiceManifest = Schema.ServiceManifest
+    type Settings = Schema.Settings
+
+    type ServiceFabricService =
+        {
+            Name : string
+            Proj : string
+            Version : string
+            Dll : string
+            Type : string
+        }
+
+    type ServiceFabricApplication =
+        {
+            Name : string
+            Version : string
+            Type : string
+            Services : ServiceFabricService []
+        }
+
+    type Creds =
+        {
+            Endpoint : string
+            PfxPath : string
+            Pass : string
+            Name : string
+            Thumbprint : string
+        }
+
+
+    let xName expandedName = XName.Get(expandedName)
+    let xElement expandedName content = XElement(xName expandedName, content |> Seq.map (fun v -> v :> obj) |> Seq.toArray) :> obj
+    let xAttribute expandedName value = XAttribute(xName expandedName, value) :> obj
+
+    let serviceMani (service : ServiceFabricService) =
+        xElement "ServiceManifest" [
+            xAttribute "Name" service.Name
+            xAttribute "Version" service.Version
+            //xAttribute "xmlns" "http://schemas.microsoft.com/2011/01/fabric"
+            //xAttribute "xmlns:xsd" "http://www.w3.org/2001/XMLSchema"
+            //xAttribute "xmlns:xsi" "http://www.w3.org/2001/XMLSchema-instance"
+            xElement "ServiceTypes" [
+                xElement "StatelessServiceType" [
+                    xAttribute "ServiceTypeName" service.Type
+                ]
+            ]
+            xElement "CodePackage" [
+                xAttribute "Name" "Code"
+                xAttribute "Version" service.Version
+                xElement "EntryPoint" [
+                    xElement "ExeHost" [
+                        xAttribute "IsExternalExecutable" true
+                        xElement "Program" ["dotnet"]
+                        xElement "Arguments" [service.Dll]
+                    ]
+                ]
+            ]
+            xElement "ConfigPackage" [
+                xAttribute "Name" "Config"
+                xAttribute "Version" service.Version
+            ]
+        ]
+
+    let appMani (application : ServiceFabricApplication) =
+        xElement "ApplicationManifest" [
+            xAttribute "ApplicationManifestName" application.Name
+            xAttribute "ApplicationManifestVersion" application.Version
+            //xAttribute "xmlns" "http://schemas.microsoft.com/2011/01/fabric"
+            //xAttribute "xmlns:xsd" "http://www.w3.org/2001/XMLSchema"
+            //xAttribute "xmlns:xsi" "http://www.w3.org/2001/XMLSchema-instance"
+            xElement "ServiceManifestImport" [
+                xElement "ServiceManifestRef" [
+                    xAttribute "ServiceManifestName" application.Services.[0].Name
+                    xAttribute "ServiceManifestVersion" application.Services.[0].Version
+                ]
+            ]
+        ]
+
+    let configMani =
+        xElement "Settings" [
+            //xAttribute "xmlns" "http://schemas.microsoft.com/2011/01/fabric"
+            //xAttribute "xmlns:xsd" "http://www.w3.org/2001/XMLSchema"
+            //xAttribute "xmlns:xsi" "http://www.w3.org/2001/XMLSchema-instance"
+        ]
+
+    let serviceManifest expectoService = serviceMani expectoService :?> XElement |> ServiceManifest
+    let applicationManifest application = appMani application :?> XElement |> AppManifest
+    let settings = configMani :?> XElement |> Settings
+
+
+
+    let makeFolders (application : ServiceFabricApplication) packagePrep pkg=
+        Directory.ensure packagePrep
+        Directory.ensure (packagePrep +/+ pkg)
+        Directory.ensure (packagePrep +/+ pkg +/+ "Config")
+
+        settings.XElement.Save(packagePrep +/+ pkg +/+ "Config" +/+ "Settings.xml")
+        (serviceManifest application.Services.[0]).XElement.Save(packagePrep  +/+ pkg +/+ "ServiceManifest.xml")
+        (applicationManifest application).XElement.Save(packagePrep +/+ "ApplicationManifest.xml")
+
+        Directory.ensure (packagePrep +/+ pkg +/+ "Code")
+
+
+    let getCreds (creds : Creds) ct =
+            let clientCert = new System.Security.Cryptography.X509Certificates.X509Certificate2(creds.PfxPath, creds.Pass)
+            let name = X509Name(creds.Name,creds.Thumbprint)
             let remoteSecuritySettings = RemoteX509SecuritySettings(List<X509Name>([name]))
             Task.FromResult<SecuritySettings>(X509SecuritySettings(clientCert, remoteSecuritySettings))
 
-    let connect cluster getcreds =
+    let connect creds =
         let sfClient = ServiceFabricClientBuilder()
-                        .UseEndpoints(Uri(@"https://"+cluster+":19080"))
-                        .UseX509Security(Func<CancellationToken,Task<SecuritySettings>>(getcreds))
+                        .UseEndpoints(Uri(creds.Endpoint))
+                        .UseX509Security(Func<CancellationToken,Task<SecuritySettings>>(getCreds creds))
                         .BuildAsync().GetAwaiter().GetResult();
         sfClient
 
     let push (client : IServiceFabricClient) path storePath =
         let ct = CancellationToken()
-        client.ImageStore.UploadApplicationPackageAsync(path,true,storePath,ct)
+        client.ImageStore.UploadApplicationPackageAsync(path,true,storePath,ct).GetAwaiter().GetResult()
+
+    let start (client : IServiceFabricClient) appType appName =
+        let ct = CancellationToken()
+        let appParams = Dictionary<string, string>()
+        let appDesc = Microsoft.ServiceFabric.Common.ApplicationDescription(Microsoft.ServiceFabric.Common.ApplicationName("fabric:/" + ""), appType, "1.0.0", appParams);
+        client.Applications.CreateApplicationAsync(appDesc,Nullable<int64>(),ct)
+
+    let provision (client : IServiceFabricClient) appType =
+        client.ApplicationTypes.ProvisionApplicationTypeAsync(Microsoft.ServiceFabric.Common.ProvisionApplicationTypeDescription(appType))
+
 
